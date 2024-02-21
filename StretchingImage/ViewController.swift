b6a0f25181d34adfb83c60cb40584e9caabeed2c
temasarkisov <a.sarkisov.s@yandex.ru>
2024-02-21 18:15:34 +0200

    Initial Commit

---
+++ StretchingImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchingImage
+//
+//  Created by Артем Саркисов on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
